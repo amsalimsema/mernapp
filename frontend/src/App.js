5309b88d4ca8a038253960e5b67ee49cb2a5ca10
amsalimsema <salimsema@yahoo.com>
2022-07-10 09:43:28 +0300

    BackendAndFrontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// pages and components
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="pages">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>&copy;{new Date().getFullYear()}&nbsp;Workout Buddy</p>
+    </div>
+  )
+}
+export default Footer
